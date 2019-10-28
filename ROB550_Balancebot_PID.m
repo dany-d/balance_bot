264749443cada9cef0d69e1af67fd198016e9c2b
Haochen Wu <chenaaron@0587406127.wireless.umich.net>
2019-10-28 14:10:38 -0400

    pid

--- ROB550_Balancebot_PID.m
+++ ROB550_Balancebot_PID.m
@@ -12,16 +12,16 @@ R       = .04;          % radius of wheel in m MEASURED
 L       = .100;         % center of wheel to Center of mass (TO BE DETERMINED)
 L       = .076;         % measured 7.6cm
 I_r     = 0.004;        % Inertia of body about center (not wheel axis) Kg*m^2 (TO BE DETERMINED)
-I_r     = 0.004559740881941055; % measured Jzz
+I_r     = 0.0038100019065888755; % measured Jxx
 g       = 9.81;         % gravity m/s^2
 R_gb    = 20.4;         % gearbox ratio
 tau_s   = 0.50;         % Motor output stall Torque @ V_nominal (TO BE DETERMINED)
-tau_s   = 1.9864;       % measured left motor
+tau_s   = 2.08715;       % measured left motor
 w_free  = 50;           % Motor output free run speed @ V_nominal (TO BE DETERMINED)
-w_free  = 33.0682;      % measured left motor
+w_free  = 43.42535;      % measured left motor
 V_n     = 12.0;         % motor nominal drive voltage
 I_gb = 100.0*10^-5;     % inertial of motor armature and gearbox (TO BE DETERMINED)
-I_gb = 8.84*10^-5;      % measured average
+I_gb = 4.46*10^-5;      % measured average
 
 % add inertia of wheels modeled as disks and times two for both sides
 I_w = 2 * (I_gb+(m_w*R^2)/2);
@@ -65,32 +65,44 @@ tr1 = 0.05; %desired rise time of inner loop
 wc1 = 1.8/tr1; %rule of thumb crossover freq
 gain = 1.0;
 Kp1 = -3.0;
-Kp1 = -1.2;
+Kp1 = -4.5;
+Ki1 = -10.0;
 Ki1 = -30.0;
 Kd1 = -0.05;
-Kd1 = -0.1;
+Kd1 = -0.05;
 D1 = gain*pid(Kp1, Ki1, Kd1, 1/wc1);
 OL1 = minreal(D1*G1);
 CL1 = feedback(OL1,1);
 figure
 step(CL1)
+axis([0,1,0,1.5])
 stepinfo(CL1)
 
 %% Outer loop position controller
 tr2 = 0.35; % desired rise time of outer loop
 wc2 = 1.8/tr2;
+gain2 = 0.5;
 Kp2 = 0.04;
-Kp2 = 0.05;
+Kp2 = 0.01;
 Ki2 = 0.005;
-Ki2 = 0.05;
+Ki2 = 0.001;
 Kd2 = 0.015;
-Kd2 = 0.0115;
-D2 = pid(Kp2, Ki2, Kd2, 1/wc2)
+Kd2 = 0.03;
+
+gain2 = 1;
+Kp2 = -0.005;
+Ki2 = -0.00008;
+Kd2 = -0.015;
+
+
+D2 = gain2 * pid(Kp2, Ki2, Kd2, 1/wc2)
 OL2 = minreal(D2*CL1*G2);
 CL2 = feedback(OL2,1);
 figure
 step(CL2)
+axis([0,10,-0.5,2])
 stepinfo(CL2)
+
 %% find discrete controller for inner loop
 D1z = c2d(D1,DT,'tustin');
 tf(D1z)
